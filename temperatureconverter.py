bc596f054d689d6e7ecd8ab30ead9a8285b1be90
SadiqovAzizbek <asadiqov244@gmail.com>
2025-10-07 11:52:42 +0500

    Initial commit: Create interactive hello world program

---
+++ temperatureconverter.py
@@ -0,0 +1,8 @@
+Celsius = float(input("Enter Temperature in Celsius: "))
+print(f"\n{'='*35}")
+Fahrenheit = str(Celsius * 9/5 + 32 )
+Kelvin = str(Celsius + 273.15)
+print(f"{Celsius} in Celsius")
+print(f"{Fahrenheit} in Celsius")
+print(f"{Kelvin} in Celsius")
+
