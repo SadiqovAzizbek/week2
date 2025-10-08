e11c0abfa9b31620decd0b9807c88d4e922ce7c5
SadiqovAzibek <asadiqov244@gmail.com>
2025-10-08 09:14:32 +0500

    week2 practice (calculator)

---
+++ calculator.py
@@ -0,0 +1,8 @@
+pi = 3.14159
+r = float(input("Enter The Radius : "))
+print(f"\n{'='*35}")
+Area = ( pi * r * r )
+Circumference = ( 2 * pi * r )
+print(f"{r} in Radius")
+print(f"{Area} in Radius ")
+print(f"{Circumference} in Radius ")
